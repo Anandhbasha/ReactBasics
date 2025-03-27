4848a8a69709f9556b91260926c8263f4acd19f6
Anandhbasha <anadhakm@gmail.com>
2025-03-27 10:20:26 +0530

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,92 @@
-// // function App(){
+// // // function App(){
+// // //   return(
+// // //     <div className="app">
+// // //       <h1>Hello</h1>
+// // //       <h1>Welcome</h1>
+// // //     </div>
+// // //   )
+// // // }
+// // // export default App;
+
+// // const App = ()=>{
 // //   return(
-// //     <div className="app">
+// //         <div className="app">
+// //           <h1>Hello</h1>
+// //           <h1>Welcome</h1>
+// //         </div>
+// //       )
+// // }
+
+// // export default App
+
+// // import React from 'react'
+
+// // const App = () => {
+// //   return (
+// //      <div className="app">       
 // //       <h1>Hello</h1>
 // //       <h1>Welcome</h1>
-// //     </div>
+// //      </div>
 // //   )
 // // }
-// // export default App;
 
-// const App = ()=>{
-//   return(
-//         <div className="app">
-//           <h1>Hello</h1>
-//           <h1>Welcome</h1>
-//         </div>
-//       )
-// }
-
-// export default App
+// // export default App
+// const cardDetails = [{
+//   cardTitle:"",
+//   cardImage:"",
+//   cardText:""
+// },{
+//   cardTitle:"",
+//   cardImage:"",
+//   cardText:""
+// },{
+//   cardTitle:"",
+//   cardImage:"",
+//   cardText:""
+// }]
 
 // import React from 'react'
+// import Button from './Button'
 
 // const App = () => {
+//   const btnValue = ["Click","Submit","Login"]
 //   return (
-//      <div className="app">       
-//       <h1>Hello</h1>
-//       <h1>Welcome</h1>
-//      </div>
+//     <div className='App'>
+//       {btnValue.map((item)=>(<Button value = {item}/>))}
+      
+//     </div>
 //   )
 // }
 
 // export default App
-const cardDetails = [{
-  cardTitle:"",
-  cardImage:"",
-  cardText:""
-},{
-  cardTitle:"",
-  cardImage:"",
-  cardText:""
-},{
-  cardTitle:"",
-  cardImage:"",
-  cardText:""
-}]
+
+
 
 import React from 'react'
-import Button from './Button'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 const App = () => {
-  const btnValue = ["Click","Submit","Login"]
+  let [count,setCount] = useState(0)
+  let [bg,setBg] = useState("dark")
+
+  useEffect(()=>{
+    console.log("UseEffect is working");
+    
+  },[count])
+  const countClick =()=>{
+    setCount(++count)
+    console.log(count);
+    
+  }
+  const bgChange = ()=>{
+    setBg(bg==="dark"?"light":"dark")
+  }
   return (
     <div className='App'>
-      {btnValue.map((item)=>(<Button value = {item}/>))}
-      
+      <p>{count}</p>
+      <button onClick={countClick}>Count</button>
+      <button onClick={bgChange} style={{backgroundColor:bg==="dark"?"black":"white",color:bg==="dark"?"white":"black"}}>{bg}</button>
     </div>
   )
 }
